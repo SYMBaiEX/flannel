32f3cb316070d525f5912c7900cf71d6680fd737
SYMBaiEX <solsymbaiex@gmail.com>
2026-06-28 14:41:03 -0500

    Initial Commit

---
+++ flannelTests/flannelTests.swift
@@ -0,0 +1,19 @@
+//
+//  flannelTests.swift
+//  flannelTests
+//
+//  Created by SYMBiEX on 6/28/26.
+//
+
+import Testing
+@testable import flannel
+
+struct flannelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
